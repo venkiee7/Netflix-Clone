72e9ed3071ec27b61bfec8721f364b42d6414d05
Vyankatesh Jaju <jajuvyankatesh2@gmail.com>
2023-01-06 18:17:55 +0530

    Endpoints

---
+++ netflix-clone/src/common/endpoints.ts
@@ -0,0 +1,6 @@
+export const ENDPOINT= {
+    MOVIES_POPULAR:"movie/popular?language=en-IN&page=1&region=IN",
+    MOVIES_TOP_RATED:"movie/top_rated?language=en-IN&page=1&region=IN",
+    MOVIES_NOW_PLAYING:"movie/now_playing?language=en-IN&page=1&region=IN",
+    MOVIES_VIDEO:"movie/{movie_id}/videos",
+}
